e5166f32ed014f4056706eb8b7dbffbf70007ee9
Md Shakil Hossain <shakilshifat65@gmail.com>
2025-10-22 07:14:39 +0600

    feat(store): add StoreProvider component for Redux integration

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,22 +1,16 @@
 'use client';
 
 import store from '@/redux/store';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { SessionProvider } from 'next-auth/react';
 import { Provider } from 'react-redux';
 
-import QueryProvider from '@/lib/providers/query-provider';
+import QueryProvider from '@/lib/providers/StoreProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
       <QueryProvider>
-        <Provider store={store}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            {children}
-          </LocalizationProvider>
-        </Provider>
+        <Provider store={store}>{children}</Provider>
       </QueryProvider>
     </SessionProvider>
   );
