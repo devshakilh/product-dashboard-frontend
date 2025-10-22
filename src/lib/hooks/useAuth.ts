c44e95a1e2aae503e754679de9a6243db8a60112
Md Shakil Hossain <shakilshifat65@gmail.com>
2025-10-22 07:07:26 +0600

    feat(store): configure Redux store with auth and products APIs

---
+++ src/lib/hooks/useAuth.ts
@@ -0,0 +1,16 @@
+// 'use client';
+
+// import { useSelector } from 'react-redux';
+
+// import { RootState } from '@/lib/store';
+
+// export const useAuth = () => {
+//   const { user, isAuthenticated } = useSelector(
+//     (state: RootState) => state.auth
+//   );
+
+//   return {
+//     user,
+//     isAuthenticated,
+//   };
+// };
