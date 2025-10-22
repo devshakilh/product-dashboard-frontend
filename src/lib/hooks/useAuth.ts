c6b67da269f392f5cf10324f784b7012f4e1750a
Md Shakil Hossain <shakilshifat65@gmail.com>
2025-10-22 07:08:00 +0600

    feat(hooks): add useAuth hook for accessing authentication state

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -1,16 +1,16 @@
-// 'use client';
+'use client';
 
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-// import { RootState } from '@/lib/store';
+import { RootState } from '@/lib/store';
 
-// export const useAuth = () => {
-//   const { user, isAuthenticated } = useSelector(
-//     (state: RootState) => state.auth
-//   );
+export const useAuth = () => {
+  const { user, isAuthenticated } = useSelector(
+    (state: RootState) => state.auth
+  );
 
-//   return {
-//     user,
-//     isAuthenticated,
-//   };
-// };
+  return {
+    user,
+    isAuthenticated,
+  };
+};
